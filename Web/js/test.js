817d247f8212989740a34a4ed955247be99b6953
Vasy228123007 <60486871+Vasy228123007@users.noreply.github.com>
2020-08-27 15:34:14 +0300

    Обновление
    1.CS Добавили в css .chat_form
    2.HTML Добавили блок char_form,info
    3.JS Добавили вывод на экран доктора и добавили проверку на то ввел ли человек номер

--- Web/js/test.js
+++ Web/js/test.js
@@ -2,20 +2,30 @@ $(function() {
 
 let nomer = $("#nomer");
 let likar;
-let all_likar = ["Чума Светлана","Зубенко Михайло"];
+let all_likar = ["Чума Светлана","Зубенко Михайло","Зуб Степан","Голомозий Петро","Кортопле Олег"];
 
 $("#send").click(function() { 
 
+if(nomer.val() == "")
+	{alert("Ви не ввели номер телефону")}
+else
+	{
 console.log("Ваш номер: "+nomer.val());
 $(".likar_form").show();
 $(".phone_form").hide();
-return false; });
+return false;
+	}
+});
 
 $("#sendlikar").click(function() { 
 
 likar=$("#likar").val();
-console.log("Ваш лікар: "+all_likar[likar]);
+//console.log("Ваш лікар: "+all_likar[likar]);
  
+$(".chat_form").show();
+$(".likar_form").hide();
+$(".info").html("Ваш лікар: <b>"+all_likar[likar]+"</b>");
+
 return false; });
 
 });
